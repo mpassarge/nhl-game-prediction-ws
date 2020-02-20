23292e51ba3e69068eee8b972e248957339ce7c6
Michael Passarge <mpassarge@comcast.net>
2020-02-20 10:46:19 -0500

    Added date validations, return res when necessary

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Prediction = require('../models/Prediction');
+const moment = require('moment');
 
 // TODO: adjust path of post/get
 router.post('/', (req, res) => {
 
     const date = req.body.date;
+
+    // Validate date format
+    if(!moment(date, "YYYY-MM-DD", true).isValid()) {
+        return res.status(400).json({"error": "Invalid date format. Use YYYY-MM-DD."});
+    }
 
     Prediction.findOne({date: date}).exec()
         .then((prediction) => {
@@ -19,35 +25,41 @@ router.post('/', (req, res) => {
                     .then((newPrediction) => {
                         // TODO: Go over what is being returned back to client
                         // TODO: should return location of new prediction
-                        res.status(200).json(prediction);
+                        return res.status(200).json(prediction);
                     })
                     .catch((err) => {
                         console.error(err);
-                        res.status(500).send();
+                        return res.status(500).send();
                     });
             } else {
-                res.status(400).send({"error": "Date " + date + " already submitted"});
+                return res.status(400).send({"error": "Date " + date + " already submitted"});
             }
         })
         .error((err) => {
             console.error(err);
-            res.status(500).send("Error ");
+            return res.status(500).send("Error ");
         });
 });
 
 router.get('/', (req, res) => {
     const date = req.query.date;
+
+    // Validate date format
+    if(!moment(date, "YYYY-MM-DD", true).isValid()) {
+        return res.status(400).json({"error": "Invalid date format. Use YYYY-MM-DD."});
+    }
+
     Prediction.findOne({date: date}).exec()
         .then((prediction) => {
             if(prediction != null) {
-                res.status(200).json(prediction);
+                return res.status(200).json(prediction);
             } else {
-                res.status(404).send({"error": "Cannot find prediction with date " + date});
+                return res.status(404).send({"error": "Cannot find prediction with date " + date});
             }
         })
         .catch((err) => {
             console.error(err);
-            res.status(500).send({"error": "Error trying to find prediction with date " +  date});
+            return res.status(500).send({"error": "Error trying to find prediction with date " +  date});
         });
 });
 
